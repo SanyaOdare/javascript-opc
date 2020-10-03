65f98c026e39c79caf57b7f2d1268bc4e38f71c7
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-03 08:11:02 +0100

    add writable.js file

--- demo.js
+++ demo.js
@@ -1,23 +1,18 @@
 'use strict'; 
 (function() {
 
-  let person1 = {
-    firstName: 'Sanya',
-    lastName: 'Odare',
+  let person = {
+    name: {
+      firstName: 'Sanya',
+    lastName: 'Odare'
+    },    
     age: 29
   };
 
-  let person2 = Object.create(
-    Object.prototype,
-    {
-      firstName: {value: 'Sanya', enumerable:true, writable:true, configurable:true},
-      lastName: {value: 'Odare', enumerable:true, writable:true, configurable:true},
-      age: {value:29, enumerable:true, writable:true, configurable:true},
-    }
-  );
+  Object.defineProperty(person, 'name', {writable: false});
 
-  display(person1);
-  display(person2);
+  person.name.firstName = 'Alani';
 
+  display(person.name);
 
 })();
---
+++ writable.js
@@ -0,0 +1,18 @@
+// 'use strict'; 
+// (function() {
+
+//   let person = {
+//     name: {
+//       firstName: 'Sanya',
+//     lastName: 'Odare'
+//     },
+//     age: 29
+//   };
+
+//   Object.defineProperty(person, 'name', {writable: false});
+
+//   person.name.firstName = 'Alani';
+
+//   display(person.name);
+
+// })();
