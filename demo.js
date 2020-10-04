9ebc5e8fdca5a4e7bb1243c6be3bc9599e2852b2
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-04 17:21:09 +0100

    add prototype.js file

--- demo.js
+++ demo.js
@@ -1,12 +1,22 @@
 'use strict'; 
 (function() {
 
-  let person = {
-    firstName: 'Sanya',
-    lastName: 'Odare',
-    age: 29
-  };
+  function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
+  Person.prototype.age = 29;
 
+  display(Person.prototype);
+
+  let alani = new Person('Alani', 'Odare');
+  let asake = new Person('Asake', 'Odare');
+  asake.__proto__.age = 19;
+
+  display(alani.__proto__);
+  display(asake.__proto__);
+  
+  display(Person.prototype === alani.__proto__);
 
 })();
