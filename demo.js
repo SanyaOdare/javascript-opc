1ee520c003c91aa9328c22bdaa08457d98a8c891
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-05 16:16:19 +0100

    add math-object.js file

--- demo.js
+++ demo.js
@@ -1,55 +1,8 @@
 'use strict'; 
 (function() {
 
-  class Person {
-    constructor(firstName, lastName, age) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.age = age;
-    }
+  display(Math.PI);
+  display(Math.max(2, 42, 29));
+  display(Math.round(29.6));
 
-    static adultAge = 18;
-
-    get fullName() {
-      return this.firstName + ' ' + this.lastName;
-    }
-
-    set fullName(fullName) {
-      var nameParts = fullName.split(' ');
-      this.firstName = nameParts[0];
-      this.lastName = nameParts[1];
-    }
-
-    isAdult() {
-      return this.age >= 18;
-    }
-  }
-
-  display(Person.adultAge);
-
-  class Student extends Person {
-    constructor(firstName, lastName, age) {
-      super(firstName, lastName, age);
-      this._enrolledCourses = [];
-    }
-
-    static fromPerson(person) {
-      return new Student(person.firstName, person.lastName, person.age);
-    }
-
-    enroll(courseId) {
-      this._enrolledCourses.push(courseId);
-    }
-
-    getCourses() {
-      return this.fullName + "'s enrolled courses are: " +
-        this._enrolledCourses.join(', ');
-    }
-  }
-
-  let alani = new Person('Alani', 'Odare', 29);
-
-  let alaniStudent = Student.fromPerson(alani);
-
-  display(alaniStudent);
 })();
---
+++ math-object.js
@@ -0,0 +1,8 @@
+// 'use strict'; 
+// (function() {
+
+//   display(Math.PI);
+//   display(Math.max(2, 42, 29));
+//   display(Math.round(29.6));
+
+// })();
