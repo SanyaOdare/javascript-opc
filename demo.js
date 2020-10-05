5b7fd31d615293ec20ac377ae0b25c514bf6bff6
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-05 17:57:42 +0100

    add regex.js file

--- demo.js
+++ demo.js
@@ -1,25 +1,12 @@
 // 'use strict'; 
 // (function() {
 
-//   let date1 = new Date(2050,3,25,13,1,30,50);
-//   let date2 = new Date(2051,3,25,13,1,30,55);
+//   function checkPasswordComplexity(password) {
+//     let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/gi;
 
-//   display(date2 - date1);
+//     return regex.test(password);
+//   }
 
-  // display(date.getFullYear());
-  // display(date.getMonth()); //Zero-based
-  // display(date.getDate());
-  // display(date.getHours());
-  // display(date.getMinutes());
-  // display(date.getSeconds());
-  // display(date.getMilliseconds());
-
-  // display(date.getUTCFullYear());
-  // display(date.getUTCMonth()); //Zero-based
-  // display(date.geUTCtDate());
-  // display(date.geUTCtHours());
-  // display(date.geUTCtMinutes());
-  // display(date.getUTCSeconds());
-  // display(date.getUTCMilliseconds());
+//   display(checkPasswordComplexity('Stronger1'));
 
 // })();
---
+++ regex.js
@@ -0,0 +1,12 @@
+// 'use strict'; 
+// (function() {
+
+//   function checkPasswordComplexity(password) {
+//     let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/gi;
+
+//     return regex.test(password);
+//   }
+
+//   display(checkPasswordComplexity('Stronger1'));
+
+// })();
