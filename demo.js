1f2f3ba071b6b25ad881ae3bdeef696bbb929bfb
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-05 17:27:48 +0100

    add dates-with-date-object.js file

--- demo.js
+++ demo.js
@@ -1,8 +1,25 @@
-'use strict'; 
-(function() {
+// 'use strict'; 
+// (function() {
 
-  display(Math.PI);
-  display(Math.max(2, 42, 29));
-  display(Math.round(29.6));
+//   let date1 = new Date(2050,3,25,13,1,30,50);
+//   let date2 = new Date(2051,3,25,13,1,30,55);
 
-})();
+//   display(date2 - date1);
+
+  // display(date.getFullYear());
+  // display(date.getMonth()); //Zero-based
+  // display(date.getDate());
+  // display(date.getHours());
+  // display(date.getMinutes());
+  // display(date.getSeconds());
+  // display(date.getMilliseconds());
+
+  // display(date.getUTCFullYear());
+  // display(date.getUTCMonth()); //Zero-based
+  // display(date.geUTCtDate());
+  // display(date.geUTCtHours());
+  // display(date.geUTCtMinutes());
+  // display(date.getUTCSeconds());
+  // display(date.getUTCMilliseconds());
+
+// })();
