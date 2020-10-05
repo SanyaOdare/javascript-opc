47c3bbc7485e7a2126ed13f10d083104e424bf21
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-05 15:41:16 +0100

    add constructor-function.js file

--- constructor-function.js
+++ constructor-function.js
@@ -1,52 +1,52 @@
-'use strict'; 
-(function() {
+// 'use strict'; 
+// (function() {
 
-  function Person(firstName, lastName, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.isAdult = function() {
-      return this.age >= 18;
-    }
+//   function Person(firstName, lastName, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.isAdult = function() {
+//       return this.age >= 18;
+//     }
 
-    Object.defineProperty(this, 'fullName', {
-      get: function() {
-        return this.firstName + ' ' + this.lastName + '' + this.age;
-      },
-      set: function(fullName) {
-        var nameParts = fullName.split(' ');
-        this.firstName = nameParts[0];
-        this.lastName = nameParts[1];
-      },
-      enumerable: true
-    });
-  }
+//     Object.defineProperty(this, 'fullName', {
+//       get: function() {
+//         return this.firstName + ' ' + this.lastName + '' + this.age;
+//       },
+//       set: function(fullName) {
+//         var nameParts = fullName.split(' ');
+//         this.firstName = nameParts[0];
+//         this.lastName = nameParts[1];
+//       },
+//       enumerable: true
+//     });
+//   }
 
-  function Student(firstName, lastName, age) {
-    Person.call(this, firstName, lastName, age);
-    this._enrolledCourses = [];
+//   function Student(firstName, lastName, age) {
+//     Person.call(this, firstName, lastName, age);
+//     this._enrolledCourses = [];
 
-    this.enroll = function(courseId) {
-      this._enrolledCourses.push(courseId);
-    };
+//     this.enroll = function(courseId) {
+//       this._enrolledCourses.push(courseId);
+//     };
 
-    this.getCourses = function() {
-      return this.fullName + "'s enrolled courses are: " +
-        this._enrolledCourses.join(', ');
-    };
-  }
-  Student.prototype = Object.create(Person.prototype);
-  Student.prototype.constructor = Student;
-  Student.fromPerson = function(person) {
-    return new Student(person.firstName, person.lastName, person.age);
-  }
+//     this.getCourses = function() {
+//       return this.fullName + "'s enrolled courses are: " +
+//         this._enrolledCourses.join(', ');
+//     };
+//   }
+//   Student.prototype = Object.create(Person.prototype);
+//   Student.prototype.constructor = Student;
+//   Student.fromPerson = function(person) {
+//     return new Student(person.firstName, person.lastName, person.age);
+//   }
 
 
-  let alani = new Student('Alani', 'Odare', 29);
-  alani.enroll('CS205');
-  alani.enroll('MA101');
-  alani.enroll('PS101');
+//   let alani = new Student('Alani', 'Odare', 29);
+//   alani.enroll('CS205');
+//   alani.enroll('MA101');
+//   alani.enroll('PS101');
 
-  display(alani.getCourses);
+//   display(alani.getCourses);
 
-})();
+// })();
